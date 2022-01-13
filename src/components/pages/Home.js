ee9f7ff8ccbc7152347b4c42a3ebb658c203c4b4
Raunak Agarwal <71973645+Raunak173@users.noreply.github.com>
2022-01-13 19:13:27 +0530

    fixed some bugs

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,13 @@
-import '../../App.css';
-import HeroSection from '../HeroSection';
-import React from 'react';
+import "../../App.css";
+import HeroSection from "../HeroSection";
+import React from "react";
 
-
-function Home () {
-    return(
-        <>
-          <HeroSection/>
-        </>
-    );
-};
+function Home() {
+  return (
+    <>
+      <HeroSection />
+    </>
+  );
+}
 
 export default Home;
